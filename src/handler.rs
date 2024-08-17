8f0df40322a94e74b5b907e92980d75e3f79043e
mi-wada <mtak.wd@gmail.com>
2024-08-17 09:37:39 +0900

    feat: Check DB Health at Health endpoint

--- src/handler.rs
+++ src/handler.rs
@@ -4,15 +4,24 @@ use crate::env;
 
 #[derive(Clone)]
 pub(crate) struct AppState {
-    env: env::Env,
+    pub(crate) env: env::Env,
+    pub(crate) db_pool: sqlx::PgPool,
 }
 
 impl AppState {
-    pub(crate) fn new(env: env::Env) -> Self {
-        Self { env }
+    pub(crate) fn new(env: env::Env, db_pool: sqlx::PgPool) -> Self {
+        Self { env, db_pool }
     }
 }
 
 pub(crate) async fn get_healthz(State(state): State<AppState>) -> impl IntoResponse {
-    StatusCode::OK
+    if sqlx::query("SELECT 1")
+        .fetch_one(&state.db_pool)
+        .await
+        .is_err()
+    {
+        StatusCode::INTERNAL_SERVER_ERROR
+    } else {
+        StatusCode::OK
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use axum::{extract::State, http::StatusCode, response::IntoResponse, routing::get, Router};
+use axum::{routing::get, Router};
+use sqlx::postgres::PgPoolOptions;
 
 mod env;
 mod handler;
@@ -8,13 +9,21 @@ async fn main() {
     env::Env::init();
     let env = env::Env::new();
 
+    let db_pool = PgPoolOptions::new()
+        .max_connections(5)
+        .idle_timeout(std::time::Duration::from_secs(5))
+        .acquire_timeout(std::time::Duration::from_secs(5))
+        .connect(&env.database_url)
+        .await
+        .expect("Failed to connect to Postgres");
+
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", env.port))
         .await
         .unwrap();
     let app = Router::new()
         // curl -v -X GET http://localhost:8080/healthz
         .route("/healthz", get(handler::get_healthz))
-        .with_state(handler::AppState::new(env));
+        .with_state(handler::AppState::new(env, db_pool));
 
     axum::serve(listener, app).await.unwrap();
 }
