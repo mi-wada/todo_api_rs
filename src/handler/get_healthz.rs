77cf4380c26439c95bc06d4ee4d1d5eaadaee5aa
mi-wada <mtak.wd@gmail.com>
2024-08-20 20:57:27 +0900

    refactor(health endpoint): Separate db_healthy method

--- src/handler/get_healthz.rs
+++ src/handler/get_healthz.rs
@@ -3,13 +3,13 @@ use axum::{extract::State, http::StatusCode, response::IntoResponse};
 use super::AppState;
 
 pub(crate) async fn get_healthz(State(state): State<AppState>) -> impl IntoResponse {
-    if sqlx::query("SELECT 1")
-        .fetch_one(&state.db_pool)
-        .await
-        .is_err()
-    {
+    if db_healthy(&state.db_pool).await {
         StatusCode::INTERNAL_SERVER_ERROR
     } else {
         StatusCode::OK
     }
+}
+
+async fn db_healthy(db_pool: &sqlx::PgPool) -> bool {
+    sqlx::query("SELECT 1").fetch_one(db_pool).await.is_ok()
 }
